48c0317f5642527cb572b301840c838cbc76e2c5
root <root@jenkinsserver.jlab.com>
2017-08-01 06:46:38 -0700

    adjust

--- src/Rectangulator.java
+++ src/Rectangulator.java
@@ -1,13 +1,12 @@
 public class Rectangulator {
-	public stactic void main(String[] args) {
-		int length = Integer.parseInt(args[0]);
-		int width = Integer.parseInt(args[0]);
+  public static void main(String[] args) {
+    int length = Integer.parseInt(args[0]);
+    int width = Integer.parseInt(args[1]);
 
-		Rectangle myRectangle = new Rectangle(lenght, width);
+    Rectangle myRectangle = new Rectangle(length, width);
 
-		String output = String.format("*** Your Rectangle ***\n\nLenght: %d\nWidth: %d\nArea: %d\nPerimeter: %d\n\", myRectangle.leght, myRectangle.width, myRectangle.getArea(), myRectangle.getPerimeter());
+    String output = String.format("*** Your Rectangle ***\n\nLength: %d\nWidth: %d\nArea: %d\nPerimeter: %d\n\n", myRectangle.length, myRectangle.width, myRectangle.getArea(), myRectangle.getPerimeter());
 
-		System.out.println(output);
-	}	
-
+    System.out.println(output);
+  }
 }
